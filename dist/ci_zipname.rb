7f9bcffbdd57d51ef1dbe43db1e5eb24b5633129
soburi <soburi@gmail.com>
2016-07-02 08:42:35 +0900

    travis env

--- dist/ci_zipname.rb
+++ dist/ci_zipname.rb
@@ -1,15 +1,27 @@
 require 'git' 
 
 g = Git.open('.')
-
-gbasename = g.config['remote.origin.url'].sub(/^.*\//, "").sub(/\.git$/, "")
 lastupdate = g.log(1)[0].date.strftime( '%Y%m%d%H%M%S')
-branchname = g.branches.select {|br| br.current }[0].full
 
-if branchname =~ /\((.*)\)/
-  tmp = $1
-  tmp =~ /^(.*\ )(.*)$/
-  branchname = $2
+reponame   = 'unknown'
+branchname = 'unknown'
+
+if ENV.key?('TRAVIS_REPO_SLUG')
+  reponame = File.basename(ENV['TRAVIS_REPO_SLUG'])
+else
+  reponame = g.config['remote.origin.url'].sub(/^.*\//, "").sub(/\.git$/, "")
 end
 
-print( gbasename + '-' + branchname + '-' + lastupdate)
+if ENV.key?('TRAVIS_BRANCH')
+  branchname = ENV['TRAVIS_BRANCH']
+else 
+  branchname = g.branches.select {|br| br.current }[0].full
+  if branchname =~ /\((.*)\)/
+    tmp = $1
+    tmp =~ /^(.*\ )(.*)$/
+    branchname = $2
+  end
+end
+
+
+print(reponame + '-' + branchname + '-' + lastupdate)
