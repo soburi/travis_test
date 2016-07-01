e9aafd29b221b9a92a97622fc8e9dce8a1110022
soburi <soburi@gmail.com>
2016-07-02 08:24:28 +0900

    strip .git

--- dist/ci_zipname.rb
+++ dist/ci_zipname.rb
@@ -2,7 +2,7 @@ require 'git'
 
 g = Git.open('.')
 
-gbasename = g.config['remote.origin.url'].sub(/^.*\//, "")
+gbasename = g.config['remote.origin.url'].sub(/^.*\//, "").sub(/\.git$/, "")
 lastupdate = g.log(1)[0].date.strftime( '%Y%m%d%H%M%S')
 branchname = g.branches.select {|br| br.current }[0].full
 
