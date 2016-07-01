f6502e8ddfa040523a1af0d7bcb4f3a90e1b94fa
soburi <soburi@gmail.com>
2016-07-02 08:30:19 +0900

    detached branch

--- dist/ci_zipname.rb
+++ dist/ci_zipname.rb
@@ -6,4 +6,10 @@ gbasename = g.config['remote.origin.url'].sub(/^.*\//, "")
 lastupdate = g.log(1)[0].date.strftime( '%Y%m%d%H%M%S')
 branchname = g.branches.select {|br| br.current }[0].full
 
+if branchname =~ /\((.*)\)/
+  tmp = $1
+  tmp =~ /^(.*\ )(.*)$/
+  branchname = $2
+end
+
 print( gbasename + '-' + branchname + '-' + lastupdate)
