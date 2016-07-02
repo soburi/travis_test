a98e1a0182bb4e53beb28a1b0591624b056d0049
soburi <soburi@gmail.com>
2016-07-03 01:32:23 +0900

    setup_env

--- dist/setup_env.sh
+++ dist/setup_env.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-SETUP_ENV=hoge
+GHREPO="https://github.com/${TRAVIS_REPO_SLUG}"
+ARCHIVENAME=$(ruby dist/ci_zipname.rb)
+if [ "${TRAVIS_TAG}" == "" ] ; then
+  PKGJSON=package_$(echo ${TRAVIS_REPO_SLUG} | sed 's/\//_/')-${TRAVIS_BRANCH}_index.json
+  ARCHIVEURL="https://github.com/${TRAVIS_REPO_SLUG}/archive/${ARCHIVENAME}.zip"
+  RELEASEVER=${TRAVIS_BRANCH}-$(ruby dist/ci_lastcommit.rb)
+else
+  PKGJSON=package_$(echo ${TRAVIS_REPO_SLUG} | sed 's/\//_/')_index.json
+  ARCHIVEURL="https://github.com/${TRAVIS_REPO_SLUG}/releases/download/${TRAVIS_TAG}/${ARCHIVENAME}.tar.bz2"
+  RELEASEVER=${TRAVIS_TAG}
+fi
+
+echo GHREPO=${GHREPO}
+echo ARCHIVENAME=${ARCHIVENAME}
+echo ARCHIVEURL=${ARCHIVEURL}
+echo PKGJSON=${PKGJSON}
+echo RELEASEVER=${RELEASEVER}
