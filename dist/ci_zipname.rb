5f0dce52260f9c5b20d85cea69e8935b85baad31
soburi <soburi@gmail.com>
2016-07-02 23:34:18 +0900

    correct

--- dist/ci_zipname.rb
+++ dist/ci_zipname.rb
@@ -3,22 +3,26 @@ require 'git'
 reponame   = 'unknown'
 branchname = 'unknown'
 
-if ENV.key?('TRAVIS_REPO_SLUG')
-  reponame = File.basename(ENV['TRAVIS_REPO_SLUG'])
-else
-  reponame = g.config['remote.origin.url'].sub(/^.*\//, "").sub(/\.git$/, "")
-end
+if ENV.key?('TRAVIS_TAG')
+  if ENV.key?('TRAVIS_REPO_SLUG')
+    reponame = File.basename(ENV['TRAVIS_REPO_SLUG'])
+  else
+    reponame = g.config['remote.origin.url'].sub(/^.*\//, "").sub(/\.git$/, "")
+  end
 
-if ENV.key?('TRAVIS_BRANCH')
-  branchname = ENV['TRAVIS_BRANCH']
-else 
-  branchname = g.branches.select {|br| br.current }[0].full
-  if branchname =~ /\((.*)\)/
-    tmp = $1
-    tmp =~ /^(.*\ )(.*)$/
-    branchname = $2
+  if ENV.key?('TRAVIS_BRANCH')
+    branchname = ENV['TRAVIS_BRANCH']
+  else 
+    branchname = g.branches.select {|br| br.current }[0].full
+    if branchname =~ /\((.*)\)/
+      tmp = $1
+      tmp =~ /^(.*\ )(.*)$/
+      branchname = $2
+    end
+  end
+  print(reponame + '-' + branchname)
+else
+  if ENV.key?('TRAVIS_COMMIT')
+    print(ENV['TRAVIS_COMMIT'] + ".zip")
   end
 end
-
-
-print(reponame + '-' + branchname)
