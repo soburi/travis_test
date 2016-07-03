9645629a121eb259c1d3e136e99b7d34ae1e047e
soburi <soburi@gmail.com>
2016-07-03 10:49:04 +0900

    check existence

--- dist/setup_env.sh
+++ dist/setup_env.sh
@@ -6,11 +6,13 @@ if [ "${TRAVIS_TAG}" == "" ] ; then
   ARCHIVENAME=${TRAVIS_COMMIT}
   ARCHIVEURL="https://github.com/${TRAVIS_REPO_SLUG}/archive/${ARCHIVENAME}.zip"
   RELEASEVER=${TRAVIS_BRANCH}-$(date -d @`git log -1 ${TRAVIS_COMMIT} --pretty=medium --format=%ct` +%Y%m%d%H%M%S)
+  BM_FORCEOPT='-f'
 else
   PKGJSON=package_$(echo ${TRAVIS_REPO_SLUG} | sed 's/\//_/')_index.json
   ARCHIVENAME=$(basename ${TRAVIS_REPO_SLUG})-${TRAVIS_TAG}
   ARCHIVEURL="https://github.com/${TRAVIS_REPO_SLUG}/releases/download/${TRAVIS_TAG}/${ARCHIVENAME}.tar.bz2"
   RELEASEVER=${TRAVIS_TAG}
+  BM_FORCE=
 fi
 
 echo GHREPO=${GHREPO}
